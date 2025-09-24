82fab90f51c479ac9d600ab6dac1c1a13f1bec27
balogunkehinde13 <balogunraheem1@gmail.com>
2025-09-24 11:20:40 +0100

    created arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,14 @@
+def perform_operation(a, b, operation):
+    if operation == 'add':
+        return a + b
+    elif operation == 'subtract':
+        return a - b
+    elif operation == 'multiply':
+        return a * b
+    elif operation == 'divide':
+        if b != 0:
+            return a / b
+        else:
+            return "Error: Division by zero"
+    else:
+        return "Error: Invalid operation"
