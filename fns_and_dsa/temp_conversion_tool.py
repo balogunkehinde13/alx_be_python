92b8397951fbb00a6ef9b9967b3e0871b128fdac
balogunkehinde13 <balogunraheem1@gmail.com>
2025-09-24 12:16:32 +0100

    temp conversion

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,6 @@
 # 🌡️ Global Conversion Factors
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 # 🔄 Conversion Functions
 def convert_to_celsius(fahrenheit: float) -> float:
